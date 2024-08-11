3c80c51bd0170360d12d02835c3bd9bd4d9c0097
Aswath <aswathsivanp@gmail.com>
2024-08-11 22:31:47 +0530

    building game card

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <>
       <Grid templateAreas={{
-        base: `"nav main"`,
-        lg: `"nav nav" "aside main"`
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
       }}>
         <GridItem area="nav"><Navbar></Navbar></GridItem>
         <Show above='lg'>
