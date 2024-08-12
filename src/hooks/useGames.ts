317b7cb35d96e61b27025311486e268e738d6265
Aswath <aswathsivanp@gmail.com>
2024-08-12 21:52:26 +0530

    adding platform icons to game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  readonly id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   readonly id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 interface FetchGameResponse {
   count: number;
