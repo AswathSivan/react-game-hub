75a61fb9769d8703b531189b5249e7bb2d0d32af
AswathSivan <aswathsivanp@gmail.com>
2024-09-27 20:38:37 +0530

    Api call for game list

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   params: {
-    api_key: import.meta.env.VITE_API_KEY,
+    key: import.meta.env.VITE_API_KEY,
   },
 });
 
