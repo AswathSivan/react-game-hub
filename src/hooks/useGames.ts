46a7704c7d33b422e08960109af43e7875443c51
AswathSivan <aswathsivanp@gmail.com>
2024-09-27 22:07:38 +0530

    game card created and listed

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,18 +1,21 @@
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
+
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+}
+interface FetchGameResponse {
+  count: number;
+  results: Game[];
+}
+
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState();
 
-  interface Game {
-    id: number;
-    name: string;
-  }
-  interface FetchGameResponse {
-    count: number;
-    results: Game[];
-  }
   useEffect(() => {
     const controller = new AbortController();
     apiClient
