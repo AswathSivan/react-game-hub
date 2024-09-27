32a5cb3630afe1cbac9b3891b45568bf76457f4a
AswathSivan <aswathsivanp@gmail.com>
2024-09-27 20:03:49 +0530

    Added colormode switch and axios config

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
-import { HStack, Image, Text } from "@chakra-ui/react"
+import { HStack, Image } from "@chakra-ui/react"
 import Logo from '../assets/logo.png'
+import ColorModeSwitch from "./ColorModeSwitch"
 const Navbar = () => {
     return (
-        <HStack>
+        <HStack justifyContent="space-between" p={3}>
             <Image src={Logo} boxSize="50px" bg={"white"} borderRadius='full' padding={1} />
-            <Text>Game Hub</Text>
+            <ColorModeSwitch />
         </HStack>
     )
 }
