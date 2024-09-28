97036f08994ac1bb2aadd95633c7c68dc9294f58
AswathSivan <aswathsivanp@gmail.com>
2024-09-28 17:54:53 +0530

    platform list component

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,7 +15,7 @@ const GameGrid = ({ selectedGenre }: Props) => {
     return (
         <>
             {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={3} padding={10}>
+            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={3} padding="10px">
                 {isLoading && skeletons.map(skeleton => (<GameCardContainer key={skeleton}><GameCardSkeleton /></GameCardContainer>))}
                 {data.map(game => (<GameCardContainer key={game.id}><GameCard game={game} /></GameCardContainer>))}
             </SimpleGrid>
