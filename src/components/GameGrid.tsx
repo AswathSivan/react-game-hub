9123bbe81d7e55d113086214530ef02166a16e66
AswathSivan <aswathsivanp@gmail.com>
2024-09-28 15:57:05 +0530

    sorting using genre sidenav implemented

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,15 +3,21 @@ import useGames from "../hooks/useGames"
 import GameCard from "./GameCard"
 import GameCardSkeleton from "./GameCardSkeleton"
 import GameCardContainer from "./GameCardContainer"
-const GameGrid = () => {
-    const { data, error, isLoading } = useGames()
+import { Genre } from "../hooks/useGenres"
+
+interface Props {
+    selectedGenre: Genre | null
+}
+
+const GameGrid = ({ selectedGenre }: Props) => {
+    const { data, error, isLoading } = useGames(selectedGenre)
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
     return (
         <>
             {error && <Text>{error}</Text>}
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={3} padding={10}>
-                {isLoading && skeletons.map(skeleton => (<GameCardContainer><GameCardSkeleton key={skeleton} /></GameCardContainer>))}
-                {data.map(game => (<GameCardContainer><GameCard key={game.id} game={game} /></GameCardContainer>))}
+                {isLoading && skeletons.map(skeleton => (<GameCardContainer key={skeleton}><GameCardSkeleton /></GameCardContainer>))}
+                {data.map(game => (<GameCardContainer key={game.id}><GameCard game={game} /></GameCardContainer>))}
             </SimpleGrid>
         </>
     )
